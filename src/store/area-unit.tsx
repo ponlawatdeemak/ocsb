d2fb3188448c843614bec35b68aebaf056c7a8de
Warutniy <warut.n@mapboss.co.th>
2025-01-21 18:04:41 +0700

    fix: update area unit store

--- src/store/area-unit.tsx
+++ src/store/area-unit.tsx
@@ -1,23 +1,23 @@
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
-import { AreaUnit } from '@/enum'
+import { AreaUnitKey } from '@/enum'
 
 type StoreAreaUnit = {
-	areaUnit: AreaUnit
-	setAreaUnit: (areaUnit: AreaUnit) => void
+	areaUnit: AreaUnitKey
+	setAreaUnit: (areaUnit: AreaUnitKey) => void
 }
 
 const useAreaUnit = create(
 	persist<StoreAreaUnit>(
 		(set) => ({
-			areaUnit: AreaUnit.Rai,
-			setAreaUnit: (areaUnit: AreaUnit) =>
+			areaUnit: AreaUnitKey.Rai,
+			setAreaUnit: (areaUnit: AreaUnitKey) =>
 				set(() => {
 					return { areaUnit: areaUnit }
 				}),
 		}),
 		{
-			name: 'sugarcaneAreaUnit', // name of the item in the storage (must be unique)
+			name: 'areaUnit', // name of the item in the storage (must be unique)
 			storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
 		},
 	),
