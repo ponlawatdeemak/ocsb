0419185221dace5f55768c52db5c856a5bf636c7
ThunyautN <thunyavut.n@mapboss.co.th>
2025-01-24 12:00:59 +0700

    add: log-in background image

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -5,7 +5,7 @@ interface AuthLayoutProps extends PropsWithChildren {}
 export default function AuthLayout({ children }: AuthLayoutProps) {
 	return (
 		<div className='flex h-full w-full flex-col'>
-			<div className='flex flex-1 flex-col overflow-auto bg-[url("/images/sugar-cane-tree.jpg")] bg-cover bg-center'>
+			<div className='flex flex-1 flex-col overflow-auto bg-[url("/images/login-bg.png")] bg-cover bg-center'>
 				{children}
 			</div>
 		</div>
