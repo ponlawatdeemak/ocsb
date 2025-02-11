2e83c5b29abfc0e996bc9af059ecc4916d474f65
ThunyautN <thunyavut.n@mapboss.co.th>
2025-02-11 18:20:02 +0700

    add: translations, icon

--- src/components/svg/MenuIcon.tsx
+++ src/components/svg/MenuIcon.tsx
@@ -344,3 +344,24 @@ export const MapCompassIcon: React.FC<SVGProps> = ({ width = 40, height = 40, fi
 		</svg>
 	)
 }
+
+export const InfoIcon: React.FC<SVGProps> = ({ width = 15, height = 15, fill = 'white' }) => {
+	return (
+		<svg width={width} height={height} viewBox='0 0 15 15' fill='none' xmlns='http://www.w3.org/2000/svg'>
+			<g clip-path='url(#clip0_1150_14153)'>
+				<path
+					d='M7.5 10V7.5M7.5 5H7.50625M13.75 7.5C13.75 10.9518 10.9518 13.75 7.5 13.75C4.04822 13.75 1.25 10.9518 1.25 7.5C1.25 4.04822 4.04822 1.25 7.5 1.25C10.9518 1.25 13.75 4.04822 13.75 7.5Z'
+					stroke={fill}
+					stroke-width='2'
+					stroke-linecap='round'
+					stroke-linejoin='round'
+				/>
+			</g>
+			<defs>
+				<clipPath id='clip0_1150_14153'>
+					<rect width='15' height='15' fill={fill} />
+				</clipPath>
+			</defs>
+		</svg>
+	)
+}
