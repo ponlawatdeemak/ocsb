5624f07409684f0de315629cacd55fd11d4f8ee6
Tua'D <tua.developer@gmail.com>
2025-02-11 08:16:50 +0700

    fix: build prod

--- src/components/pages/profile/Main/index.tsx
+++ src/components/pages/profile/Main/index.tsx
@@ -98,7 +98,7 @@ export const ProfileMain: React.FC<ProfileMainProps> = ({ className = '' }) => {
 	})
 
 	const defaultFormValues: UMFormValues = {
-		image: null,
+		image: '',
 		firstName: '',
 		lastName: '',
 		position: null,
@@ -147,7 +147,7 @@ export const ProfileMain: React.FC<ProfileMainProps> = ({ className = '' }) => {
 				if (values.image) {
 					let formData = new FormData()
 					formData.append('file', values.image)
-					await service.um.postImage(values.image, {
+					await service.um.postImage(values.image as File, {
 						userId: profileData?.data.userId ?? '',
 					})
 				}
