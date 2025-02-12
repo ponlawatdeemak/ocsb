0e48b5804a9dc0b7ffc75f7e017d0821b50b1f47
Tua'D <tua.developer@gmail.com>
2025-02-13 01:16:23 +0700

    update temp

--- src/components/pages/map-analyze/burnt-area/Main/index.tsx
+++ src/components/pages/map-analyze/burnt-area/Main/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 interface BurntAreaMainProps {
-    className?: string
+	className?: string
 }
 
-export const BurntAreaMain: React.FC<BurntAreaMainProps> = ({ className = ''}) => {
-    return (
-        <div>BurntAreaMain</div>
-    )
+export const BurntAreaMain: React.FC<BurntAreaMainProps> = ({ className = '' }) => {
+	return (
+		<div>
+			<div className='flex flex-col text-center'>
+				<div className='text-[200px]'>🏗️</div>
+				<div className='text-[50px]'>🔥🔥🔥</div>
+				<div>Under Development!</div>
+			</div>
+		</div>
+	)
 }
