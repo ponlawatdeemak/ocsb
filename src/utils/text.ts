f3d4f36a5f62670563fc972a4170072fad214a18
tua_d <tua.developer@gmail.com>
2025-02-21 05:13:34 +0700

    fix: validation error

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -3,7 +3,7 @@ export function defaultText(text?: any, defaultValue: any = '') {
 }
 
 export function defaultNumber(value: number | string, digit: number = 2) {
-	return value !== '' && value !== null
+	return value || value === 0
 		? value.toLocaleString(undefined, { maximumFractionDigits: digit, minimumFractionDigits: 0 })
 		: ''
 }
