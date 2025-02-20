41d33626b827a2b5fc018f98d415969f2a48de78
ThunyautN <thunyavut.n@mapboss.co.th>
2025-02-21 02:13:00 +0700

    add: translations and api functions

--- src/api/map-analyze/index.tsx
+++ src/api/map-analyze/index.tsx
@@ -1,8 +1,17 @@
 import { api } from '@/api/core'
 import { ResponseDto } from '@interface/config/app.config'
+import { GetDashBoardBurntAreaDtoIn } from '@interface/dto/brunt-area/brunt-area.dto-in'
+import { GetDashBoardBurntAreaDtoOut } from '@interface/dto/brunt-area/brunt-area.dto.out'
 
 const mapAnalyze = {
 	getBurnAreaCalendar: async (): Promise<ResponseDto<string[]>> => await api.get('/brunt-area/burn-area-calendar'),
+	getDashBoardBurntArea: async (
+		payload: GetDashBoardBurntAreaDtoIn,
+	): Promise<ResponseDto<GetDashBoardBurntAreaDtoOut>> => {
+		return await api.get(
+			`/brunt-area/dashboard?startDate=${payload.startDate}&endDate=${payload.endDate}${payload.admC ? `&admC=${payload.admC}` : ''}${payload.mapType ? `&mapType=${payload.mapType}` : ''}${payload.inSugarcan ? `&inSugarcan=${payload.inSugarcan}` : ''}`,
+		)
+	},
 }
 
 export default mapAnalyze
