3707338b74e7199033b0e49e18a528011d95c177
ThunyautN <thunyavut.n@mapboss.co.th>
2025-02-28 18:02:21 +0700

    fix: FilterSelect optionUnit and extends BaseSelectProps

--- src/components/common/select/FilterSelect.tsx
+++ src/components/common/select/FilterSelect.tsx
@@ -1,5 +1,5 @@
 import { ExpandMore } from '@mui/icons-material'
-import { FormControl, MenuItem, Select, SelectChangeEvent } from '@mui/material'
+import { BaseSelectProps, FormControl, MenuItem, Select, SelectChangeEvent } from '@mui/material'
 import classNames from 'classnames'
 import React from 'react'
 
@@ -8,8 +8,9 @@ export interface FilterSelectOptionType {
 	name: string
 }
 
-interface FilterSelectProps {
+interface FilterSelectProps extends BaseSelectProps<any> {
 	className?: string
+	optionsClassName?: string
 	id: string
 	required?: boolean
 	fullWidth?: boolean
@@ -17,11 +18,13 @@ interface FilterSelectProps {
 	data: FilterSelectOptionType[]
 	placeholder?: string
 	disabled?: boolean
+	optionUnit?: string
 	onChange: (event: SelectChangeEvent) => void
 }
 
 const FilterSelect: React.FC<FilterSelectProps> = ({
 	className = '',
+	optionsClassName,
 	id,
 	required = false,
 	fullWidth = true,
@@ -29,11 +32,14 @@ const FilterSelect: React.FC<FilterSelectProps> = ({
 	disabled = false,
 	value,
 	data,
+	optionUnit,
 	onChange,
+	...props
 }) => {
 	return (
 		<FormControl fullWidth={fullWidth} required={required} className={classNames('', className)}>
 			<Select
+				{...props}
 				id={`${id}-select`}
 				className='h-[38px] !rounded-[5px] bg-white p-0 [&_input]:h-[38px] [&_svg]:right-3 [&_svg]:top-[calc(50%-12px)] [&_svg]:h-6 [&_svg]:w-6 [&_svg]:text-black'
 				IconComponent={ExpandMore}
@@ -44,7 +50,8 @@ const FilterSelect: React.FC<FilterSelectProps> = ({
 					slotProps: {
 						paper: {
 							className:
-								'mt-1 border-[0.5px] border-solid border-gray p-1 bg-white !rounded-[5px] shadow-[0_3.43px_2.75px_0_rgba(0,0,0,0.02),0_8.69px_6.95px_0_rgba(0,0,0,0.03)]',
+								'mt-1 border-[0.5px] border-solid border-gray p-1 bg-white !rounded-[5px] shadow-[0_3.43px_2.75px_0_rgba(0,0,0,0.02),0_8.69px_6.95px_0_rgba(0,0,0,0.03)]' +
+								` ${optionsClassName}`,
 						},
 					},
 					MenuListProps: {
@@ -63,7 +70,7 @@ const FilterSelect: React.FC<FilterSelectProps> = ({
 				)}
 				{data.map((item) => (
 					<MenuItem key={item.id} className='px-3 py-2 text-sm text-black sm:text-base' value={item.id}>
-						{item.name}
+						{optionUnit ? `${item.name} ${optionUnit}` : item.name}
 					</MenuItem>
 				))}
 			</Select>
