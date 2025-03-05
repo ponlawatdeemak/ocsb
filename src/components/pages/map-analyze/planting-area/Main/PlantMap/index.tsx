a9989dd2a70c1726cb082e374214d94862d33be8
tua_d <tua.developer@gmail.com>
2025-03-05 10:59:50 +0700

    add: heatmap sample

--- src/components/pages/map-analyze/planting-area/Main/PlantMap/index.tsx
+++ src/components/pages/map-analyze/planting-area/Main/PlantMap/index.tsx
@@ -154,7 +154,7 @@ const PlantingMapMain: React.FC<PlantingMapMainProps> = ({
 				}
 			})
 		}
-	}, [mapLibre, onMapExtentChange, regionData, i18n, plantingMap])
+	}, [onMapExtentChange, regionData, i18n, plantingMap])
 
 	// zoom to search area or default user region
 	useEffect(() => {
@@ -164,7 +164,7 @@ const PlantingMapMain: React.FC<PlantingMapMainProps> = ({
 				plantingMap.fitBounds(userGeometry, { padding: 100 })
 			}
 		}
-	}, [mapLibre, currentAdmOption?.geometry, session?.user?.geometry, plantingMap])
+	}, [currentAdmOption?.geometry, session?.user?.geometry, plantingMap])
 
 	const onMapClick = useCallback(
 		(info: PickingInfo) => {
@@ -185,7 +185,8 @@ const PlantingMapMain: React.FC<PlantingMapMainProps> = ({
 
 	// update layer
 	useEffect(() => {
-		if (mapLibre && plantingOverlay) {
+		if (plantingMap && plantingOverlay) {
+			//#region deck.gl layer
 			const layers = [
 				new GeoJsonLayer({
 					id: 'plant',
@@ -283,6 +284,59 @@ const PlantingMapMain: React.FC<PlantingMapMainProps> = ({
 				onClick: onMapClick,
 				getCursor: (state) => (state.isHovering ? 'pointer' : 'default'),
 			})
+			//#endregion
+
+			//#region heatmap layer
+			if (!plantingMap.getSource('earthquakes')) {
+				plantingMap.addSource('earthquakes', {
+					type: 'geojson',
+					data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+				})
+			}
+			if (!plantingMap.getLayer('earthquakes-heat')) {
+				plantingMap.addLayer(
+					{
+						id: 'earthquakes-heat',
+						type: 'heatmap',
+						source: 'earthquakes',
+						maxzoom: 9,
+						paint: {
+							// Increase the heatmap weight based on frequency and property magnitude
+							'heatmap-weight': ['interpolate', ['linear'], ['get', 'mag'], 0, 0, 6, 1],
+							// Increase the heatmap color weight weight by zoom level
+							// heatmap-intensity is a multiplier on top of heatmap-weight
+							'heatmap-intensity': ['interpolate', ['linear'], ['zoom'], 0, 1, 9, 3],
+							// Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+							// Begin color ramp at 0-stop with a 0-transparancy color
+							// to create a blur-like effect.
+							'heatmap-color': [
+								'interpolate',
+								['linear'],
+								['heatmap-density'],
+								0,
+								'rgba(33,102,172,0)',
+								0.2,
+								'rgb(103,169,207)',
+								0.4,
+								'rgb(209,229,240)',
+								0.6,
+								'rgb(253,219,199)',
+								0.8,
+								'rgb(239,138,98)',
+								1,
+								'rgb(178,24,43)',
+							],
+							// Adjust the heatmap radius by zoom level
+							'heatmap-radius': ['interpolate', ['linear'], ['zoom'], 0, 2, 9, 20],
+							// Transition from heatmap to circle layer by zoom level
+							'heatmap-opacity': ['interpolate', ['linear'], ['zoom'], 7, 1, 9, 0],
+						},
+					},
+					'custom-referer-layer',
+				)
+			}
+
+			//#endregion
 		}
 	}, [mapLibre, onMapClick, plantYieldAreaData, productYieldAreaData, replantYieldAreaData, plantingOverlay])
 
