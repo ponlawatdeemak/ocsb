40775239e53a73929370eb814edc79cb74038444
Warutniy <warut.n@mapboss.co.th>
2025-03-06 09:44:28 +0700

    fix: add capture utils

--- src/utils/capture.ts
+++ src/utils/capture.ts
@@ -1,13 +1,18 @@
 import html2canvas from 'html2canvas'
 
-export const captureMapImage = async (map: any) => {
-	if (map) {
-		const canvas = await html2canvas(map, {
+export const captureMapImage = async (divRef: React.RefObject<HTMLDivElement>) => {
+	if (!divRef.current) return null
+
+	try {
+		const canvas = await html2canvas(divRef.current, {
 			useCORS: true,
 			allowTaint: true,
 			scale: 2,
 		})
 		const dataURL = canvas.toDataURL('image/png')
 		return dataURL
+	} catch (error) {
+		console.error('Error capturing div:', error)
+		return null
 	}
 }
